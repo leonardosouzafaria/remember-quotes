16aa8aae953466dc5b99b3bc703093ca33cfbdb4
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2026-03-18 05:42:50 -0300

    Add home page menu options

--- frontend/src/app/pages/register-quote/register-quote.ts
+++ frontend/src/app/pages/register-quote/register-quote.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { InputQuote } from "../../components/input-quote/input-quote";
-import { TuiButton, TuiGroup } from '@taiga-ui/core';
+import { TuiGroup } from '@taiga-ui/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { TuiChip } from "@taiga-ui/kit";
+
 
 @Component({
   selector: 'app-register-quote',
-  imports: [ReactiveFormsModule, InputQuote, TuiGroup],
+  imports: [ReactiveFormsModule, InputQuote, TuiGroup, TuiChip],
   templateUrl: './register-quote.html',
   styleUrl: './register-quote.scss',
 })
 export class RegisterQuote {
+   protected readonly menuOptions = [
+    { label: 'Cadastrar livro', targetRoute: 'edit' },
+    { label: 'Livros cadastrados', targetRoute: 'source' },
+    { label: 'Citações cadastradas', targetRoute: 'source' }
+  ];
   quoteForm = new FormGroup({ quoteText: new FormControl() });
 }
